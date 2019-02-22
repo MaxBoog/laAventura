a3f821f924685eb4b783cd3ecbb8bc8e9ecb951f
MaxBoog <max_boog@hotmail.com>
2019-02-22 23:17:21 +0100

    global site upload

---
+++ assets/js/scripts.js
@@ -0,0 +1,122 @@
+
+
+// all navbar related scripts
+
+
+// function to make the navbar collapse after click.
+
+$(document).on("click", ".navbar-collapse.in", function (e) {
+	if ($(e.target).is("a") && $(e.target).attr("class") != "dropdown-toggle") {
+		$(this).collapse("hide");
+	}
+});
+
+// function to make navbar smaller when scrolling
+
+$(window).scroll(function () {
+	if ($(document).scrollTop() > 50) {
+		$("nav").addClass("shrink");
+	} else {
+		$("nav").removeClass("shrink");
+	}
+});
+
+// javascript script to allow smooth scrolling.
+
+$(document).ready(function () {
+	$("body").scrollspy({ target: ".navbar", offset: 70 });
+
+	$("a").on("click", function (event) {
+		if (this.hash !== "") {
+			event.preventDefault();
+
+			var hash = this.hash;
+
+			$("html, body").animate(
+				{ scrollTop: $(hash).offset().top },
+				{
+					duration: 800,
+					easing: "easeOutExpo",
+					queue: false,
+					complete: function () {
+						window.location.hash = hash;
+					}
+				}
+			);
+		}
+	});
+});
+
+// scroll to section on different page when link is clicked
+
+
+
+
+
+// animations
+
+// scroll to top function
+
+$(document).ready(function () {
+	$(window).scroll(function () {
+		if ($(this).scrollTop() > 100) {
+			$(".scrollToTop").fadeIn();
+		} else {
+			$(".scrollToTop").fadeOut();
+		}
+	});
+
+	$(".scrollToTop").click(function () {
+		$("html, body").animate({ scrollTop: 0 },
+			{
+				duration: 900,
+				easing: "easeOutExpo",
+				queue: false
+			});
+		return false;
+	});
+});
+
+
+// scrollreveal
+
+window.sr = ScrollReveal();
+sr.reveal(".foo");
+
+// script for contact form.
+
+$(function () {
+	$("#contact-form").validator();
+
+	$("#contact-form").on("submit", function (e) {
+		if (!e.isDefaultPrevented()) {
+			var url = "contact.php";
+
+			$.ajax({
+				type: "POST",
+				url: url,
+				data: $(this).serialize(),
+				success: function (data) {
+					var messageAlert = "alert-" + data.type;
+					var messageText = data.message;
+
+					var alertBox =
+						'<div class="alert ' +
+						messageAlert +
+						' alert-dismissable"><button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>' +
+						messageText +
+						"</div>";
+					if (messageAlert && messageText) {
+						$("#contact-form")
+							.find(".messages")
+							.html(alertBox);
+						$("#contact-form")[0].reset();
+						grecaptcha.reset();
+					}
+				}
+			});
+			return false;
+		}
+	});
+});
+
