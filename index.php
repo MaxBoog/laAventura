9039223067a9525f9aed29019c7b773ce8aad280
MaxBoog <max_boog@hotmail.com>
2019-09-12 16:30:46 +0200

    php

--- index.php
+++ index.php
@@ -59,41 +59,9 @@
 	<!-- navigation -->
 
 	<header>
-		<nav class="navbar navbar-expand-lg fixed-top navbar-custom">
-			<div class="container">
-				<a class="navbar-brand" href="/">Carlito's Adventures</a>
-
-				<button class="navbar-toggler collapsed" type="button" data-toggle="collapse"
-					data-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false"
-					aria-label="Toggle navigation">
-					<span class="icon-bar top-bar"></span>
-					<span class="icon-bar middle-bar"></span>
-					<span class="icon-bar bottom-bar"></span>
-					<span class="sr-only">Toggle navigation</span>
-				</button>
-				<div class="collapse navbar-collapse" id="navbarResponsive">
-					<ul class="navbar-nav ml-auto">
-						<li class="nav-item active">
-							<a class="nav-link pr-md-4" href="/">home
-								<span class="sr-only">(current)</span>
-							</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link pr-md-4" href="tours.html">tours</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link pr-md-4" href="activities.html">activities</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link pr-md-4" href="service.html">services & accomodations</a>
-						</li>
-						<li class="nav-item">
-							<a class="nav-link pr-md-4" href="contact.html">contact</a>
-						</li>
-					</ul>
-				</div>
-			</div>
-		</nav>
+		<?php
+			include("nav.php")
+		?>
 	</header>
 	<section class="container">
 		<div class="row">
